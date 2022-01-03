b37ef9f127e2dde7a5e47b0cf3cefc2ec625bee3
rogs <pilique11@gmail.com>
2022-01-03 00:54:59 -0300

    GET Customer works

--- src/main/java/io/github/pedrosusername/customers/rest/CustomerController.java
+++ src/main/java/io/github/pedrosusername/customers/rest/CustomerController.java
@@ -5,6 +5,7 @@ import io.github.pedrosusername.customers.model.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/clientes")
@@ -19,4 +20,10 @@ public class CustomerController {
         return repository.save(customer);
     }
 
+    @GetMapping("{id}")
+    public Customer acharPorId( @PathVariable Integer id ){
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+    }
 }
